68137d070b285adf2b42047601629b17c63da94f
Dove <doveliu0516@gmail.com>
2023-08-02 20:16:47 +0800

    fix: update board variable

--- frontend/Board.py
+++ frontend/Board.py
@@ -5,6 +5,16 @@ from resource.frontend_config import FrontendConfig
 class Board(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        # 儲存格長度
+        self.cell_len = 40
+        # 棋盤線條數量 ( 幾乘幾的棋盤 )
+        self.line_count = 15
+        # 棋盤總長度
+        self.board_len = self.cell_len * (self.line_count + 1 )
+        # 棋盤背景色
+        self.light_yellow_color = '#FFE153'
+        #  四角 + 中間圓點的大小，數字越大，圓越大
+        self.star_point_width = 5
     
     def get_surface(self) -> pygame.Surface:
         """
@@ -12,8 +22,8 @@ class Board(pygame.sprite.Sprite):
         """
 
         # 棋盤總長度
-        board_len = FrontendConfig.BOARD_LEN
-        board_color = FrontendConfig.BOARD_COLOR
+        board_len = self.board_len
+        board_color = self.light_yellow_color
 
         board = pygame.Surface(size=(board_len, board_len))
         board.fill(board_color)
@@ -23,49 +33,53 @@ class Board(pygame.sprite.Sprite):
         """
         繪製棋盤
         """
+        black = 'black'
 
-        black = FrontendConfig.BLACK
         # 儲存格長度
-        cell_len = FrontendConfig.CELL_LEN
+        cell_len = self.cell_len
         # 棋盤線條數量 ( 幾乘幾的棋盤 )
-        line_count = FrontendConfig.LINE_COUNT
+        line_count = self.line_count
 
-        # 總共 15 行
         for i in range(1, line_count + 1):
             # board_x_list.append(i)
             # 起始座標
-            board_start = cell_len 
-            board_start_i = cell_len * i
+            start_pos = cell_len 
             # 結束座標
-            board_end = cell_len * line_count 
-            board_end_i = cell_len * i
+            end_pos = cell_len * line_count 
+            # 目前座標
+            current_pos = cell_len * i
             # print(board_x_start, board_y_start, board_x_end, board_y_end)
             line_width = 1 # 寬度
+            # 起始 & 最後一條線邊框加粗
             if i == 1 or i == line_count:
                 line_width = 3
             # x 軸 橫線
-            pygame.draw.line(surface, black, (board_start, board_start_i), (board_end, board_end_i), line_width)  
+            pygame.draw.line(surface, black, (start_pos, current_pos), (end_pos, current_pos), line_width)  
             # y 軸  直線
-            pygame.draw.line(surface, black, (board_start_i, board_start), (board_end_i, board_end), line_width)
+            pygame.draw.line(surface, black, (current_pos, start_pos), (current_pos, end_pos), line_width)
         
     def draw_star_point(self, surface:pygame.Surface):
         """
         繪製四角四個 + 中間比較粗的圓點
         """
         
-        black = FrontendConfig.BLACK
+        black = 'black'
 
         # 儲存格長度
-        cell_len = FrontendConfig.CELL_LEN
+        cell_len = self.cell_len
         # 棋盤線條數量 ( 幾乘幾的棋盤 )
-        line_count = FrontendConfig.LINE_COUNT
+        line_count = self.line_count
 
-        star_point_width = FrontendConfig.STAR_POINT_WIDTH
+        star_point_width = self.star_point_width
         star_point_len_4 = 4  * cell_len
         star_point_len_back_4 = (line_count - 3) * cell_len
         star_point_len_center = ((line_count + 1) / 2)  * cell_len
-        star_point_list = [(star_point_len_4, star_point_len_4) ,(star_point_len_4, star_point_len_back_4),
-            (star_point_len_back_4, star_point_len_4) ,(star_point_len_back_4, star_point_len_back_4),
-            (star_point_len_center, star_point_len_center)]
+        star_point_list = [
+            (star_point_len_4, star_point_len_4) ,
+            (star_point_len_4, star_point_len_back_4),
+            (star_point_len_back_4, star_point_len_4),
+            (star_point_len_back_4, star_point_len_back_4),
+            (star_point_len_center, star_point_len_center)
+        ]
         for i in range(len(star_point_list)):
             pygame.draw.circle(surface, black, star_point_list[i], star_point_width)
