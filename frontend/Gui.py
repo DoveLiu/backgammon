2c21dea493a790f7595f9bcc14dd1c980a2396e7
Dove <doveliu0516@gmail.com>
2023-08-03 13:13:32 +0800

    fix: update  gui function variable

--- frontend/Gui.py
+++ frontend/Gui.py
@@ -6,28 +6,32 @@ from resource.frontend_config import FrontendConfig
 class Gui(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.high = 720
+        self.width = 1280
+        self.light_gray_color = '#F0F0F0'
 
     def get_Surface (self) -> pygame.Surface:
         """
         獲取矩形遊戲介面物件
         """
 
-        high = FrontendConfig.GUI_HIGH
-        width = FrontendConfig.GUI_WIDTH
-        color = FrontendConfig.LIGHT_GRAY_COLOR
+        high = self.high
+        width = self.width
+        light_gray_color = self.light_gray_color
         
         screen = pygame.display.set_mode((width, high))
-        screen.fill(color)
+        screen.fill(light_gray_color)
         return screen
     
     def draw_board(self, gui_surface:pygame.Surface, board_surface:pygame.Surface):
         """
         把棋盤矩形物件，繪製到遊戲介面矩形物件上
         """
+        board = Board()
         
-        gui_high = FrontendConfig.GUI_HIGH
-        gui_width = FrontendConfig.GUI_WIDTH
-        board_len = FrontendConfig.BOARD_LEN
+        gui_high = self.high
+        gui_width = self.width
+        board_len = board.board_len
 
         board_init_width = (gui_width - board_len) / 2 
         borad_init_high = (gui_high - board_len) / 2
